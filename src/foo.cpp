ace399c99b6c1ca1896bf3e6f89d341ac3cd3bae
SwissalpS <Luke@SwissalpS.ws>
2020-09-05 01:15:25 +0200

    added some variants

--- src/foo.cpp
+++ src/foo.cpp
@@ -3,10 +3,30 @@ namespace SwissalpS { namespace FooTest { namespace IRC {
 
 
 
+// this works when run with python 3.6.9
+// but with Python 3.8.5 'irc.abort' is invisible
+
+
 /// called from lua by **core.abort**(iNumber) as **irc.abort**(iNumber)
 static int abortLua() {
 	return 0;
 } // abortLua
+
+
+// **s...** works
+
+/// called from lua by **core.bort**(iNumber) as **sirc.abort**(iNumber)
+static int bortLua() {
+	return 0;
+} // bortLua
+
+
+// **as irc.abort** also vanishes
+
+/// called from lua by **core.ort**(iNumber) **as irc.abort**(iNumber)
+static int ortLua() {
+	return 0;
+} // ortLua
 
 
 
