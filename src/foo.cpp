eeb2f78907cb9689a22b7e263bb154cef658a87d
SwissalpS <Luke@SwissalpS.ws>
2020-09-04 23:49:11 +0200

    added namespaces

--- src/foo.cpp
+++ src/foo.cpp
@@ -1,5 +1,13 @@
 
+namespace SwissalpS { namespace FooTest { namespace IRC {
+
+
+
 /// called from lua by **core.abort**(iNumber) as **irc.abort**(iNumber)
 static int abortLua() {
 	return 0;
 } // abortLua
+
+
+
+}}} // namespace SwissalpS::FooTest::IRC
